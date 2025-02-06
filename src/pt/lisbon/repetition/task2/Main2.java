861822480d42f0234b192faf16f0126e8647dff0
Dudarenko Dmitry <102149674+dudarenko-dmitry@users.noreply.github.com>
2025-02-06 14:48:00 +0000

    Task 2 complete

--- src/pt/lisbon/repetition/task2/Main2.java
+++ src/pt/lisbon/repetition/task2/Main2.java
@@ -1,4 +1,15 @@
 package pt.lisbon.repetition.task2;
 
 public class Main2 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 4, 6, 7, 3, 8, 4, 9, 2, 3};
+        int sum = 0;
+        for (int number : numbers) {
+//            System.out.println("Element " + i + " = " + numbers[i]);
+            sum = sum + number;
+        }
+        System.out.println("Sum of all elements = " + sum);
+    }
 }
